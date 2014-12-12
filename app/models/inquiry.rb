55115aeecb86d2be817fcc56c11e61784b775d82
Doug Steinberg <dstein-phins@hotmail.com>
2014-12-12 18:53:32 -0500

    mailer is working

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -4,7 +4,6 @@ class Inquiry
   attribute :name
   attribute :email
   attribute :message
-  attribute :contact_by_phone, type: Boolean
 
   validates_presence_of :name, :email, :message
   validates_format_of :email, with: /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i, multiline: true
